2f49aa7bb46cb3e893f4874b345aef8c43481b8a
BBdeve <61114333+BBdeve@users.noreply.github.com>
2021-06-01 11:04:37 +0200

    Update moje.js

--- moje.js
+++ moje.js
@@ -1,27 +1,27 @@
-const kubki = ()=>{
+const cups = ()=>{
     let heightOfCups = [];
-    const nrOfCups = prompt("podaj liczbę kubków do napełnienia:"); 
-    const kubek = '<div class="cup"><div></div></div>';
+    const nrOfCups = prompt("Please insert the number of cups to fill:"); 
+    const cup = '<div class="cup"><div></div></div>';
     const colorsOfCups = [];
     for(let i=0;i<nrOfCups;i++){
-        document.querySelector("#wrapper").innerHTML += kubek;
+        document.querySelector("#wrapper").innerHTML += cup;
         colorsOfCups.push(`rgb(${Math.floor(Math.random()*254)+1},${Math.floor(Math.random()*254)+1},${Math.floor(Math.random()*254)+1})`);
     }
-    const kubki = Array.from(document.querySelectorAll(".cup div"));
-    for (fluid in kubki){
+    const cups = Array.from(document.querySelectorAll(".cup div"));
+    for (fluid in cups){
         heightOfCups.push(0);
     }
     for(let i=0;i<nrOfCups;i++){
-        kubki[i].style=`height:${heightOfCups[i]}%; background-color: ${colorsOfCups[i]}`;
+        cups[i].style=`height:${heightOfCups[i]}%; background-color: ${colorsOfCups[i]}`;
     }
     for(let i=0;i<nrOfCups;i++){
         document.querySelectorAll(".cup")[i].addEventListener("click",()=>{
             if(heightOfCups[i] >= 90) return;
             else{
                 heightOfCups[i] += 30;
-                kubki[i].style=`height:${heightOfCups[i]}%;  background-color: ${colorsOfCups[i]}`;
+                cups[i].style=`height:${heightOfCups[i]}%;  background-color: ${colorsOfCups[i]}`;
             }
         });
     }
 }
-window.addEventListener("load",()=>{kubki();})
+window.addEventListener("load",()=>{cups();})
